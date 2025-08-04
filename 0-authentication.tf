0016192bd846227a452706134be697ee73253e2b
cooperx33 <cooperx33@gmail.com>
2025-08-04 00:28:07 -0500

    Auth

---
+++ 0-authentication.tf
@@ -0,0 +1,11 @@
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs
+provider "google" {
+  project     = "coopsclass-65"
+  region      = "us-central1"
+  credentials = "coopsclass-65-01ac4fd7170b.json"
+}
+
+# I'm doing this tonight while prepping my students for their sessions with Lizzo
+
+# comment there will be no diddy oil bootygasms here
+# Lizzo will be pegging Carlton and inducing a bootygasm deep in him with Chewbacca watching!
